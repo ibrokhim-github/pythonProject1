bdb6fa63d9f4415fa8e4c0bb4f83075bfac4fc14
Ibrokhim <ibrogim.adilov@gmailcom>
2021-11-24 12:54:16 +0300

    --global user.email "you@example.com" git config   --global user.email "ibrogim.adilov@gamil.com" git config

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+def my_function(x):
+    return 5 + x
+print(my_function(5))
+
+
+def my_function(x2):
+    return 5 * x2
+print(my_function(4))
+
+
+def my_function(x3):
+    return 10 - x3
+
+print(my_function(5))
+
+
+
+
+#
